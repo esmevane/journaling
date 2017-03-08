8cb76756f29a674808c86cbce1323d16177ebf1c
Joseph McCormick <esmevane@gmail.com>
2017-03-08 15:51:59 -0600

    Project language is Typescript.
    We'll ship our main content in `/dist` as compiled Javascript.  Unless
    this whole Typescript decision collapses like a house of cards.

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("./server");
+exports.Server = server_1.default;
---
+++ src/index.spec.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+import { Server } from "./index";
+
+describe(typeof Server, () => {
+  it("is exported", () => expect(Server).to.be.ok);
+});
